e7e3b612f2edd7c0efbd08d2c3ac32981a9bb517
vbalte <54951038+vbalte@users.noreply.github.com>
2020-01-13 11:49:09 -0500

    ok

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,19 @@
 //////email, name, graduated, require name and email, give graduated a default value
+
+const mongoose = require("mongoose");
+const User = mongoose.model("user", {
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  graduated: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = User;
